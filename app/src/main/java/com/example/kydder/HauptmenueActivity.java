729552dd4c50101ab91c2cfa67542244dd0e74e1
FinnBoyer <boyers.finn@t-online.de>
2020-03-21 17:46:30 +0100

    Verbindung zwischen Activitys

--- app/src/main/java/com/example/kydder/HauptmenueActivity.java
+++ app/src/main/java/com/example/kydder/HauptmenueActivity.java
@@ -2,13 +2,50 @@ package com.example.kydder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HauptmenueActivity extends AppCompatActivity {
+    private Button buttonSpiel;
+    private Button buttonScoreboard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonSpiel = (Button) findViewById(R.id.bSpiel);
+        buttonSpiel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSpiel();
+            }
+        });
+
+        buttonScoreboard = (Button) findViewById(R.id.bScoreboard);
+        buttonScoreboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openScoreboard();
+            }
+        });
     }
+
+
+    public void openSpiel(){
+        Intent intentSpiel = new Intent(this, SpielActivity.class);
+        startActivity(intentSpiel);
+    }
+
+
+    public void openScoreboard(){
+        Intent intentScoraboard = new Intent(this, ScoreboardActivity.class);
+        startActivity(intentScoraboard);
+    }
+
+
+
 }
