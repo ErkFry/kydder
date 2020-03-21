8ce15fb7ff4cb69e19c5fd557bcbc9ffa4654b2e
ErkFry <ErFrey@outlook.de>
2020-03-21 18:12:39 +0100

    Basic RecyclerView

--- app/src/main/java/com/example/kydder/ScoreboardActivity.java
+++ app/src/main/java/com/example/kydder/ScoreboardActivity.java
@@ -1,8 +1,15 @@
 package com.example.kydder;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class ScoreboardActivity extends AppCompatActivity {
     @Override
@@ -11,4 +18,55 @@ public class ScoreboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_scoreboard);
      }
 
+}
+
+class ScoreAdapter extends RecyclerView.Adapter<ScoreViewHolder> {
+
+
+    private ArrayList<String> mDataset;
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public ScoreAdapter(ArrayList<String> mDataset) {
+        this.mDataset = mDataset;
     }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ScoreViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.view_holder_scoreboard, parent, false);
+        ScoreViewHolder vh = new ScoreViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ScoreViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.textView.setText(mDataset.get(position));
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+}
+
+
+// Provide a reference to the views for each data item
+// Complex data items may need more than one view per item, and
+// you provide access to all the views for a data item in a view holder
+class ScoreViewHolder extends RecyclerView.ViewHolder {
+    // each data item is just a string in this case
+    public TextView textView;
+    public ScoreViewHolder(TextView v) {
+        super(v);
+        textView = v;
+    }
+}
+
+
