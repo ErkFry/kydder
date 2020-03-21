3dad59d4276742eae8318d688488c9fdf92f570d
FinnBoyer <boyers.finn@t-online.de>
2020-03-21 17:18:35 +0100

    Neue Activitys (fertig)

--- app/src/main/java/com/example/kydder/SpielActivity.java
+++ app/src/main/java/com/example/kydder/SpielActivity.java
@@ -1,6 +1,15 @@
 package com.example.kydder;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SpielActivity extends AppCompatActivity {
-}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spiel);
+     }
+
+
+    }
