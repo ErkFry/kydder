c8a1ab8de6df852fb70d82b9a3908588bed17a9d
FinnBoyer <boyers.finn@t-online.de>
2020-03-21 17:14:30 +0100

    Neue Activitys

---
+++ app/src/main/java/com/example/kydder/ScoreboardActivity.java
@@ -0,0 +1,6 @@
+package com.example.kydder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScoreboardActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/kydder/SpielActivity.java
@@ -0,0 +1,6 @@
+package com.example.kydder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SpielActivity extends AppCompatActivity {
+}
