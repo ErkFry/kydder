90dcc323f39c31fb9252f70723603e4090133d9e
ErkFry <ErFrey@outlook.de>
2020-03-22 17:01:40 +0100

    RecyclerView bissl besser

--- app/src/main/java/com/example/kydder/ScoreboardActivity.java
+++ app/src/main/java/com/example/kydder/ScoreboardActivity.java
@@ -7,11 +7,14 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ScoreboardActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,20 +26,24 @@ public class ScoreboardActivity extends AppCompatActivity {
 class ScoreAdapter extends RecyclerView.Adapter<ScoreViewHolder> {
 
 
-    private ArrayList<String> mDataset;
+    private ArrayList<String> mNames;
+    private ArrayList<String> mScores;
+    private ArrayList<String> mDates;
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public ScoreAdapter(ArrayList<String> mDataset) {
-        this.mDataset = mDataset;
+    public ScoreAdapter(String[] names, String[] scores, String[] dates) {
+        mNames = new ArrayList<>(Arrays.asList(names));
+        mScores = new ArrayList<>(Arrays.asList(scores));
+        mDates = new ArrayList<>(Arrays.asList(dates));
     }
 
     // Create new views (invoked by the layout manager)
     @Override
     public ScoreViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // create a new view
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+        ConstraintLayout layout = (ConstraintLayout) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.view_holder_scoreboard, parent, false);
-        ScoreViewHolder vh = new ScoreViewHolder(v);
+        ScoreViewHolder vh = new ScoreViewHolder(layout);
         return vh;
     }
 
@@ -45,14 +52,15 @@ class ScoreAdapter extends RecyclerView.Adapter<ScoreViewHolder> {
     public void onBindViewHolder(ScoreViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.textView.setText(mDataset.get(position));
+        holder.textView.setText(mNames.get(position));
+
 
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return mDataset.size();
+        return mNames.size();
     }
 }
 
@@ -61,12 +69,13 @@ class ScoreAdapter extends RecyclerView.Adapter<ScoreViewHolder> {
 // Complex data items may need more than one view per item, and
 // you provide access to all the views for a data item in a view holder
 class ScoreViewHolder extends RecyclerView.ViewHolder {
+
     // each data item is just a string in this case
     public TextView textView;
-    public ScoreViewHolder(TextView v) {
-        super(v);
-        textView = v;
+    public ScoreViewHolder(ConstraintLayout layout) {
+        super(layout);
     }
+
 }
 
 
