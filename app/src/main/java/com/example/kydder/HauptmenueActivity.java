a0f0d7e8928011bc93805bedf88f0943bef2dad3
ehrik <erkfry@gmail.com>
2020-05-06 01:29:43 +0200

    forgot important files

--- app/src/main/java/com/example/kydder/HauptmenueActivity.java
+++ app/src/main/java/com/example/kydder/HauptmenueActivity.java
@@ -6,6 +6,8 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class HauptmenueActivity extends AppCompatActivity {
@@ -16,6 +18,8 @@ public class HauptmenueActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         //Scoreboard initialisierung
